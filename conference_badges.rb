ab514d15777784278748e6d5dad51a4e294ff57a
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-03 03:47:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,11 +6,15 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-
+  attendees.collect do |name|
+    "Hello, my name is #{name}."
+  end
 end
 
 def assign_rooms(attendees)
-
+  attendees.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
 end
 
 def printer(attendees)
