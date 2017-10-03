ce293568d24d4fcfb6e90207ca08c8f0ba875141
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-03 03:31:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+class conference_badges(name)
+  def batch_badge_creator
+    puts "Hello, my name is #{name}."
+  end
+
+  def assign_rooms
+    puts "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
